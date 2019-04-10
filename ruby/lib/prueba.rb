ec0d43a6a043fce6dd171e07bf5f1d543d9f516e
Lautarourtiaga <34402401+Lautarourtiaga@users.noreply.github.com>
2019-04-09 23:19:10 -0300

    primer arranque a hacer variant

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -4,3 +4,15 @@ class Prueba
     :tadp
   end
 end
+
+class Object
+  def self.variant(&bool)
+      @ejecutarBool=proc &bool
+      @ejecutarExepcion= proc {
+          unless @ejecutarBool.call then
+            raise "No se pudo cumplir una condicion"
+          end
+      }
+      before_and_after_each_call(proc {},@ejecutarExepcion)
+  end
+end
