5ee80b860b7269cecae19fa2453c278e9ff638a0
Lautarourtiaga <34402401+Lautarourtiaga@users.noreply.github.com>
2019-04-14 19:32:12 -0300

    lpm me duele la cabeza

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -1,18 +1,34 @@
-class Prueba
 
-  def materia
-    :tadp
+class Class
+
+  def invariant(&bloque)
+    before_and_after_each_call(proc{},proc &bloque)
   end
+  def before_and_after_each_call(bloqueAntes,bloqueDespues)
+
+  end
+
 end
 
-class Object
-  def self.variant(&bool)
-      @ejecutarBool=proc &bool
-      @ejecutarExepcion= proc {
-          unless @ejecutarBool.call then
-            raise "No se pudo cumplir una condicion"
-          end
-      }
-      before_and_after_each_call(proc {},@ejecutarExepcion)
+
+class MiClass
+  attr_accessor :energia,:ataque
+
+  def initialize(energia,ataque)
+    self.energia=energia
+    self.ataque=ataque
   end
+
+  pre{energia>50}
+  post{ataque>50}
+  def comer ()
+    self.energia+=1
+    self.ataque+=1
+  end
+  pre{energia<50}
+  def cagar
+    self.energia-=1
+    self.ataque-=1
+  end
+
 end
