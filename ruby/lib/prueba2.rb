2cafef2dbaf3fa00cf61e39d4a96b14b1fc2c7ae
Lautarourtiaga <34402401+Lautarourtiaga@users.noreply.github.com>
2019-04-16 22:42:36 -0300

    el invariant ya funciona igual re ranciovich

--- ruby/lib/prueba2.rb
+++ ruby/lib/prueba2.rb
@@ -1,34 +1,41 @@
 
+
 class Class
 
-  def validate(&bloque)
-    condition= (proc &bloque).call
-    unless condition then raise "La condicion de algun invariant no se cumple" end
+  def nonGetterOrSetterMethods
+    variablesSinArroba=new.instance_variables.map{|var| var.to_s}.map{|var| var.gsub('@','')}
+    instance_methods(false).select do |method|
+      !( variablesSinArroba + variablesSinArroba.map{|var| var +'='}).map{|var| var.to_sym}.include? method
+    end
   end
 
   def invariant(&bloque)
-    self.instance_methods(false).select { |x| sl}.each do |name|
+    nonGetterOrSetterMethods.each do |name|
       m = instance_method(name)
       define_method(name) do |*args, &block|
-        validate &bloque
-        m.bind(self).(*args, &block)
-        validate &bloque
+        proc{unless self.instance_exec &bloque then raise "La condicion de algun invariant no se cumple" end}.call
+        result=m.bind(self).(*args, &block)
+        proc{unless self.instance_exec &bloque then raise "La condicion de algun invariant no se cumple" end}.call
+        result
       end
     end
   end
 end
+
+
+
 class MiClase
 
   attr_accessor :energia
 
   def initialize()
-    self.energia=10
+    self.energia=25
   end
-
-  #invariant{energia<30}
 
   def mensaje_1
     self.energia+=1
   end
+
+  invariant{energia<30}
 
 end
---
+++ ruby/lib/prueba3.rb
@@ -0,0 +1,17 @@
+module Interfaz
+
+  def before_and_after_each_call(bloqueAntes,bloqueAfter)
+
+  end
+
+
+end
+
+
+
+
+
+
+
+
+
