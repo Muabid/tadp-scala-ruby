dec2a4d1f48f7f336ce1c64c0a63bea25728716d
Lautarourtiaga <34402401+Lautarourtiaga@users.noreply.github.com>
2019-04-17 23:09:06 -0300

    este codigo es como el minecraft

--- ruby/lib/prueba3.rb
+++ ruby/lib/prueba3.rb
@@ -1,17 +1,38 @@
-module Interfaz
+class Class
+  def invariant(&bloqueCondicion)
+    methodAdded= self.singleton_class.instance_method(:method_added)
+    self.singleton_class.define_method(:method_added) do |*args,&bloqueMethodAdded|
 
-  def before_and_after_each_call(bloqueAntes,bloqueAfter)
-
+      result=methodAdded.bind(self.singleton_class).(*args,&bloqueMethodAdded)
+      nombreDelMetodo=args.first
+      puts nombreDelMetodo
+      m = instance_method(nombreDelMetodo)
+        define_method(nombreDelMetodo) do |*args2, &bloqueMetodo|
+          proc{unless self.instance_exec &bloqueCondicion then raise "La condicion de algun invariant no se cumple" end}.call
+          m.bind(self).(*args2, &bloqueMetodo)
+          proc{unless self.instance_exec &bloqueCondicion then raise "La condicion de algun invariant no se cumple" end}.call
+        end
+      result
+    end
   end
-
-
 end
 
+class MiClase
 
+  attr_accessor :energia
 
+  def initialize()
+    self.energia=25
+    puts "tu vieja"
+  end
 
+  invariant{energia<30}
 
+  def mensaje_1
+    self.energia+=1
+  end
 
+end
 
 
 
