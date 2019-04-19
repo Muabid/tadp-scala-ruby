be2668b97b9e4c44c9afec225ce7740ce62e824a
Lautarourtiaga <34402401+Lautarourtiaga@users.noreply.github.com>
2019-04-19 19:09:34 -0300

    sssss lorem ipsum

--- ruby/lib/prueba3.rb
+++ ruby/lib/prueba3.rb
@@ -5,24 +5,25 @@ class Class
   #Esta solucion no permite dos invariant
 
   def invariant(&bloqueCondicion)
-
+    puts "mensaje 1"
     methodAdded= self.singleton_class.instance_method(:method_added)
-
+    puts self
+    puts "mensaje 2"
     self.singleton_class.define_method(:method_added) do |*args,&bloqueMethodAdded|
       if(@@newMethod) then
         @@newMethod=false
-
-
-      methodAdded.bind(self.singleton_class).(*args,&bloqueMethodAdded)
+        puts "mensaje 3"
+      puts self
+      methodAdded.bind(self).(*args,&bloqueMethodAdded)
       nombreDelMetodo=args.first
 
       m = instance_method(nombreDelMetodo)
 
         define_method(nombreDelMetodo) do |*args2, &bloqueMetodo|
           proc{unless self.instance_exec &bloqueCondicion then raise "La condicion de algun invariant no se cumple" end}.call if nombreDelMetodo.to_s!= "initialize"
-          m.bind(self).(*args2, &bloqueMetodo)
+          result =m.bind(self).(*args2, &bloqueMetodo)
           proc{unless self.instance_exec &bloqueCondicion then raise "La condicion de algun invariant no se cumple" end}.call
-
+          result
         end
       @@newMethod=true
       end
@@ -32,22 +33,22 @@ end
 
 class MiClase
 
-  attr_accessor :energia,:gorduraDePija
+  attr_accessor :energia,:gordura
 
   invariant{energia<30}
-  invariant{gorduraDePija>5}
+  invariant{gordura>5}
 
   def initialize()
     self.energia=25
-    self.gorduraDePija=9
+    self.gordura=9
   end
 
   def subirEnegia
     self.energia+=1
   end
 
-  def agrandarPija
-    self.gorduraDePija-=1
+  def desengordar
+    self.gordura-=1
   end
 
 end
