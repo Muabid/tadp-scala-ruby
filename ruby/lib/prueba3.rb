6ac6f4b73c8ed4172703babba2442747a3610715
Lautarourtiaga <34402401+Lautarourtiaga@users.noreply.github.com>
2019-04-18 21:18:07 -0300

    jajaja es buenisimo este codigo

--- ruby/lib/prueba3.rb
+++ ruby/lib/prueba3.rb
@@ -1,18 +1,27 @@
+
 class Class
+  @@newMethod = true
+
   def invariant(&bloqueCondicion)
     methodAdded= self.singleton_class.instance_method(:method_added)
-    self.singleton_class.define_method(:method_added) do |*args,&bloqueMethodAdded|
 
-      result=methodAdded.bind(self.singleton_class).(*args,&bloqueMethodAdded)
+    self.singleton_class.define_method(:method_added) do |*args,&bloqueMethodAdded|
+      if(@@newMethod) then
+        @@newMethod=false
+      methodAdded.bind(self.singleton_class).(*args,&bloqueMethodAdded)
+
       nombreDelMetodo=args.first
       puts nombreDelMetodo
+
       m = instance_method(nombreDelMetodo)
+
         define_method(nombreDelMetodo) do |*args2, &bloqueMetodo|
           proc{unless self.instance_exec &bloqueCondicion then raise "La condicion de algun invariant no se cumple" end}.call
           m.bind(self).(*args2, &bloqueMetodo)
           proc{unless self.instance_exec &bloqueCondicion then raise "La condicion de algun invariant no se cumple" end}.call
         end
-      result
+      end
+      @@newMethod=true
     end
   end
 end
@@ -21,12 +30,11 @@ class MiClase
 
   attr_accessor :energia
 
+  invariant{energia<30}
+
   def initialize()
     self.energia=25
-    puts "tu vieja"
   end
-
-  invariant{energia<30}
 
   def mensaje_1
     self.energia+=1
