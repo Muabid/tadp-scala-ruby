f656276272f9085a4f920563268925f0bc5d6cb9
Nicolas Nobile <nnobile95@gmail.com>
2019-04-24 21:33:33 -0300

    commit intermedio tests

--- ruby/spec/pruebaFinal_spec.rb
+++ ruby/spec/pruebaFinal_spec.rb
@@ -1,37 +1,43 @@
 require 'rspec'
+
 require_relative '../lib/pruebaFinal'
+
+class MiClass
+  attr_accessor :energia,:gordura
+
+  def initialize()
+    self.energia=25
+    self.gordura=10
+  end
+  invariant{energia>5}
+  invariant{energia<30}
+
+  pre{energia>10}
+  post{energia==1}
+
+  def hacerEjercicio
+    self.energia-=1
+  end
+
+  post{gordura==10}
+
+  def comerNachos
+    self.gordura+=1
+  end
+
+  #post{|result| result==energia+litros}
+  def tomarMonster(litros)
+    self.energia+=litros
+  end
+
+end
+
+
+
 
 describe 'pruebaFinal' do
   before do
-    class MiClass
-      attr_accessor :energia,:gordura
-
-      def initialize()
-        self.energia=25
-        self.gordura=10
-      end
-      invariant{energia>5}
-      invariant{energia<30}
-
-      pre{energia>10}
-      post{energia==1}
-
-      def hacerEjercicio
-          self.energia-=1
-      end
-
-      post{gordura==10}
-
-      def comerNachos
-        self.gordura+=1
-      end
-
-      post{|result| result==energia+litros}
-      def tomarMonster(litros)
-        self.energia+=litros
-      end
-
-    end
+    # Do nothing
   end
 
   after do
@@ -39,14 +45,15 @@ describe 'pruebaFinal' do
   end
 
   context 'deberia tirar error' do
-    #it 'si se manda una instancia a hacer ejercicio por no cumplir post{energia==1}' do
-     # expect{MiClass.new.hacerEjercicio}.to raise_error(RuntimeError)
-    #end
-    #   it 'si se manda una instancia a comer nachos por no cumplir post{gordura==10}' do
-    #     #  expect{MiClass.new.comerNachos}.to raise_error(RuntimeError)
-    #     #end
-     it 'si se manda una instancia a tomar monster ya que su algoritmo no es multiplicando sino sumando' do
-      expect{MiClass.new.tomarMonster(5)}.to raise_error(RuntimeError)
-     end
+  it 'si se manda una instancia a comer nachos por no cumplir post{gordura==10}' do
+    expect{MiClass.new.comerNachos}.to raise_error(RuntimeError)
+  end
+  it 'si se manda una instancia a hacer ejercicio por no cumplir post{energia==1}' do
+      expect{MiClass.new.hacerEjercicio}.to raise_error(RuntimeError)
+    end
+
+     #it 'si se manda una instancia a tomar monster ya que su algoritmo no es multiplicando sino sumando' do
+      #expect{MiClass.new.tomarMonster(5)}.to raise_error(RuntimeError)
+     #end
   end
 end
