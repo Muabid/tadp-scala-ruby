a79f71c3c7b899011c5e4b9196cef6e69e73ff01
Muabid <bruno2698@gmail.com>
2019-04-27 00:39:43 -0300

    mi version re piol

--- ruby/lib/pruebaFinal.rb
+++ ruby/lib/pruebaFinal.rb
@@ -1,73 +1,96 @@
-class ProcAndContext
-  attr_accessor :proc,:contexto
-  def initialize(proc)
-    self.proc=proc
-  end
-  def executeInContext
-    contexto.instance_eval &proc
-  end
-end
-
-class Class
+module Contract
   @@newMethod = true
-  @@invariantBlocks=[ProcAndContext.new(proc{true})]
-  @@validation= proc{|procAndContext|
-    unless procAndContext.executeInContext then raise "Some contract is being broken" end}
-  @@preBlock=ProcAndContext.new(proc{true})
-  @@postBlock=ProcAndContext.new(proc{true})
 
-  def defineMethodAdded
-    self.singleton_class.define_method(:method_added) do |*args,&bloqueMethodAdded|
-      if(@@newMethod) then
-        @@newMethod=false
-        nombreDelMetodo=args.first
-        m = instance_method(nombreDelMetodo)
-        pasoPrevio=@@preBlock
-        pasoPosterior=@@postBlock
-        @@preBlock=ProcAndContext.new(proc{true})
-        @@postBlock=ProcAndContext.new(proc{true})
-        define_method(nombreDelMetodo) do |*args2, &bloqueMetodo|
+  def self.extended(mod)
+    mod.class_variable_set :@@befores, []
+    mod.class_variable_set :@@afters, []
+  end
 
-          pasoPrevio.contexto=self
-          @@validation.call(pasoPrevio) if nombreDelMetodo.to_s!= "initialize" #precondicion
+  def before_and_after_each_call(before, after,klass = ContractType)
 
-          @@invariantBlocks.each{|procAndContext|
-            procAndContext.contexto=self
-            @@validation.call(procAndContext) if nombreDelMetodo.to_s!= "initialize"} #invariant
+    befores = self.class_variable_get(:@@befores)
+    afters = self.class_variable_get(:@@afters)
 
-          result =m.bind(self).(*args2, &bloqueMetodo) #definicion del metodo
+    befores.push(klass.new(before))
+    afters.push(klass.new(after))
 
-          @@invariantBlocks.each{|procAndContext|
-            procAndContext.contexto=self
-            @@validation.call(procAndContext)} #invariant
+    self.define_singleton_method(:method_added) do |method|
+      befores.last.instance_variable_set :@method,method
+      afters.last.instance_variable_set :@method,method
 
-          pasoPosterior.contexto=self
-          @@validation.call(pasoPosterior) #postcondicion
-
+      if (@@newMethod) then
+        @@newMethod = false
+        m = instance_method(method.to_s)
+        define_method(method) do |*args, &block|
+          befores.each {|x| x.call(self,method)}
+          result = m.bind(self).(*args, &block)
+          afters.each {|x| x.call(self,method,result)}
           result
         end
-        @@newMethod=true
+        @@newMethod = true
       end
     end
   end
 
-  def pre(&bloqueCondicion)
-      @@preBlock=ProcAndContext.new(bloqueCondicion)
-      defineMethodAdded
+  def invariant(&block)
+    before_and_after_each_call(proc {true}, block,Invariant)
   end
 
-  def post(&bloqueCondicion)
-    @@postBlock=ProcAndContext.new(bloqueCondicion)
-    defineMethodAdded
+  def pre(&block)
+    before_and_after_each_call(block,proc{true},PrePost)
   end
 
+  def post(&block)
+    before_and_after_each_call(proc{true},block,PrePost)
+  end
 
-  def invariant(&bloqueCondicion)
-    @@invariantBlocks.push(ProcAndContext.new(bloqueCondicion))
-    defineMethodAdded
+  class ContractType
+    attr_accessor :proc,:method
+
+    def initialize(proc)
+      @proc = proc
+    end
+
+    def call(object,method,*args)
+      object.instance_exec(*args,&@proc)
+    end
+  end
+
+  class Invariant < ContractType
+    def call(object,method,*args)
+      result = super(object,method)
+      unless result then
+        raise "No se cumple el invariant"
+      end
+    end
+  end
+
+  class PrePost < ContractType
+    def call(object,method,*args)
+      if method.equal? @method then
+        unless super(object,method,*args) then
+          raise "ASFJAJFJSAI"
+        end
+      end
+    end
   end
 end
 
+class Clase
+  extend Contract
+  attr_accessor :energia
 
+  def initialize
+    @energia = 10
+  end
+
+  post {|res| res >10}
+  def hola
+    @energia = 18
+  end
+
+end
+
+puts Clase.new.hola
 
 
